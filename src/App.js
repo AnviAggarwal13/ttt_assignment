abb4f86bce5ad962157cfdc4a2e015694538f69a
AnviAggarwal13 <anviaggarwal13@gmail.com>
2023-05-16 00:49:48 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import {React, useState} from 'react';
 import './App.css';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import {CSVLink} from 'react-csv';
 
 function App() {
+  const[buttonClick, setButtonClick]= useState(false);
+  const[data, setData] =useState("");
+  const[freq, setFreq] =useState({});
+  const[top, setTop]= useState([]);
+  const[histogram, setHistogram] = useState([]);  
+
+  const generateData = (top, freq) =>{
+    const data = top.map(word=>({
+      word, frequency: freq[word],
+    }));
+
+    setHistogram(data);
+  }
+
+  const csvData=top.map(word=>({
+    Word:word,
+    Frequency:freq[word],
+   }));
+
+  const handleClick = () =>{
+    fetch('https://www.terriblytinytales.com/test.txt')
+    .then(response=>response.text())
+    .then(data=>{
+      setData(data);
+      const words = data.split(' ');
+      const frequency = words.reduce((init, word)=>{
+        init[word]=(init[word] || 0)+1;
+        return init;
+      },{});
+      setFreq(frequency);
+
+      const sort= Object.entries(frequency).sort((a,b)=> b[1]-a[1]);
+      const top20 = sort.slice(0, 20).map(([word])=>word);
+      setTop(top20);
+
+      generateData(top20, frequency);
+      setButtonClick(true);
+
+    })
+    .catch(error=>console.log(error))
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!buttonClick && <button id="submit" onClick={handleClick}>SUBMIT</button>}
+      {buttonClick && histogram.length>0 && (
+        <>
+      <BarChart width={800} height={400} data={histogram}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="word" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey="frequency" fill="#088f8f"></Bar>
+     </BarChart>
+
+    
+     <CSVLink data={csvData} filename="histogram_data.csv">
+      <button id="export">Export CSV</button>
+     </CSVLink>
+        </>
+      )}
+        
     </div>
   );
 }
